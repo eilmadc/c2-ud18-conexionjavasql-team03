7f3199c12069e47e6e14d2ea2422e85ce3b597fd
eilmadc@gmail.com <eilmadc@gmail.com>
2023-03-29 13:48:45 +0200

    clear

--- c2-ud18-conexionjavasql-team03/src/database/Database.java
+++ c2-ud18-conexionjavasql-team03/src/database/Database.java
@@ -4,10 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.logging.Logger;
 
-import com.mysql.cj.MysqlConnection;
-import com.mysql.cj.x.protobuf.MysqlxNotice.Warning.Level;
 
 public class Database {
 //-----------------------------------------------------------------
