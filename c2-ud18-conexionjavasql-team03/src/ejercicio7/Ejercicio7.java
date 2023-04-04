e5a6cf7bbd84be11c1a1c056be10b187c82db457
eilmadc@gmail.com <eilmadc@gmail.com>
2023-04-04 21:38:06 +0200

    Update Ejercicio7.java

--- c2-ud18-conexionjavasql-team03/src/ejercicio7/Ejercicio7.java
+++ c2-ud18-conexionjavasql-team03/src/ejercicio7/Ejercicio7.java
@@ -36,14 +36,16 @@ public class Ejercicio7 {
 
 		// --Cadena que contiene los atributos de la tablas
 		String atributosCientificos = "DNI varchar(8) NOT NULL, NOMBREAPELLIDOS varchar(255) NOT NULL, PRIMARY KEY (DNI)";
-		String atributosProyecto = "ID varchar(4) NOT NULL, NOMBRE varchar(255) NOT NULL, HORAS int";	
-		String atributosAsignado = "CIENTIFICO varchar(8) NOT NULL, PROYECTO varchar(4) NOT NULL,"
-				+ " PRIMARY KEY(CIENTIFICO,PROYECTO), "
+		String atributosProyecto = "ID varchar(4) NOT NULL, NOMBRE varchar(255) NOT NULL, HORAS int,PRIMARY KEY (ID) ";	
+		String atributosAsignado = "CIENTIFICO varchar(8) NULL,"
+				+ "KEY (CIENTIFICO),"
+				+ " PROYECTO varchar(4) NULL,"
+				+ "KEY(PROYECTO),"
 				+ "FOREIGN KEY (CIENTIFICO) REFERENCES cientificos(DNI) ON DELETE CASCADE ON UPDATE CASCADE,"
 				+ "FOREIGN KEY (PROYECTO) REFERENCES proyecto(ID) ON DELETE CASCADE ON UPDATE CASCADE";
 		
 		// --datos
-		String cientifico1 = ("'1111111A', 'Cientifico 1'");
+		String cientifico1 = ("'7777777A', 'Cientifico 1'");
 		String cientifico2 = ("'2222222B', 'Cientifico 2'");
 		String cientifico3 = ("'3333333C', 'Cientifico 3'");
 		String cientifico4 = ("'4444444D', 'Cientifico 4'");
@@ -53,16 +55,16 @@ public class Ejercicio7 {
 		String proyecto3 = ("'P003','Proyecto 003', '2200'");
 		String proyecto4 = ("'P004','Proyecto 004', '65200'");
 		String proyecto5 = ("'P005','Proyecto 005', '1200'");
-		String asignado1 = ("'7777777A','1'");
-		String asignado2 = ("'2222222B','2'");
-		String asignado3 = ("'3333333C','3'");
-		String asignado4 = ("'4444444D','4'");
-		String asignado5 = ("'5555555E','5'");
+		String asignado1 = ("'7777777A','P001'");
+		String asignado2 = ("'2222222B','P002'");
+		String asignado3 = ("'3333333C','P003'");
+		String asignado4 = ("'4444444D','P004'");
+		String asignado5 = ("'5555555E','P005'");
 
 		// --Creacion tablas: cientificos-proyecto-asignado_a, con sus atributos
 		db.createTable("ejercicio7", "cientificos", atributosCientificos, conexion);
 		db.createTable("ejercicio7", "proyecto", atributosProyecto, conexion);
-		db.createTable("ejercicio7", "asignado_a", atributosAsignado, conexion);
+	
 		
 		// --1.CREATE DATA:insercion de datos en las tablas: cientificos
 		db.insertData("ejercicio7", "cientificos", cientifico1, conexion);
@@ -76,12 +78,15 @@ public class Ejercicio7 {
 		db.insertData("ejercicio7", "proyecto", proyecto3, conexion);
 		db.insertData("ejercicio7", "proyecto", proyecto4, conexion);
 		db.insertData("ejercicio7", "proyecto", proyecto5, conexion);
+		db.createTable("ejercicio7", "asignado_a", atributosAsignado, conexion);
+		
 		// --1.CREATE DATA:insercion de datos en las tablas: asignado
 		db.insertData("ejercicio7", "asignado_a", asignado1, conexion);
 		db.insertData("ejercicio7", "asignado_a", asignado2, conexion);
 		db.insertData("ejercicio7", "asignado_a", asignado3, conexion);
 		db.insertData("ejercicio7", "asignado_a", asignado4, conexion);
 		db.insertData("ejercicio7", "asignado_a", asignado5, conexion);
+		
 
 		// --2. READ DATA: CREATE DATA:Recogemos el resultado
 		ResultSet rsCientificos = db.getValues("ejercicio7", "cientificos", conexion);
@@ -93,23 +98,21 @@ public class Ejercicio7 {
 
 			utils.imprime("CIENTIFICOS");
 			while (rsCientificos.next()) {
-				System.out.println("\nCodigo: " + rsCientificos.getInt("CODIGO"));
-				System.out.println("Nombre: " + rsCientificos.getString("NOMBRE"));
-				System.out.println("Calificacion Edad: " + rsCientificos.getString("CALIFICACIONEDAD"));
+				System.out.println("\nCodigo: " + rsCientificos.getString("DNI"));
+				System.out.println("Nombre: " + rsCientificos.getString("NOMBREAPELLIDOS"));
 			}
 			
 			utils.imprime("PROYECTO");
 			while (rsProyecto.next()) {
-				System.out.println("\nCodigo: " + rsProyecto.getInt("CODIGO"));
+				System.out.println("\nCodigo: " + rsProyecto.getString("ID"));
 				System.out.println("Nombre: " + rsProyecto.getString("NOMBRE"));
-				System.out.println("Calificacion Edad: " + rsProyecto.getString("CALIFICACIONEDAD"));
+				System.out.println("Calificacion Edad: " + rsProyecto.getString("HORAS"));
 			}
 
 			utils.imprime("ASIGNADO_A");
 			while (rsAsignado.next()) {
-				System.out.println("\nCodigo: " + rsAsignado.getInt("CODIGO"));
-				System.out.println("Nombre: " + rsAsignado.getString("NOMBRE"));
-				System.out.println("Pelicula: " + rsAsignado.getString("PELICULA"));
+				System.out.println("\nCodigo: " + rsAsignado.getString("CIENTIFICO"));
+				System.out.println("Nombre: " + rsAsignado.getString("PROYECTO"));
 			}
 			utils.imprimeLinea();
 		} catch (SQLException e) {
