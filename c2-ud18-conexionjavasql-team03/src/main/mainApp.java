a9d5deadb814adb8a6701b20454f22dc0a20c7dd
Arnau <Arnau@Javier>
2023-04-05 19:31:34 +0200

    final

--- c2-ud18-conexionjavasql-team03/src/main/mainApp.java
+++ c2-ud18-conexionjavasql-team03/src/main/mainApp.java
@@ -6,8 +6,6 @@ import java.sql.SQLException;
 
 import database.Database;
 import ejercicio3.Ejercicio3;
-import ejercicio6.Ejercicio6;
-import ejercicio9.Ejercicio9;
 
 public class mainApp {
 
@@ -17,12 +15,8 @@ public class mainApp {
 		//Connection conexion = null;
 		//conexion = db.openConnection(conexion);
 		
-//		Ejercicio3 e3 = new Ejercicio3();
-//		e3.eje3Inicia();
-//		Ejercicio6 e6 = new Ejercicio6();
-//		e6.eje6Inicia();
-		Ejercicio9 e9 = new Ejercicio9();
-		e9.eje9Inicia();
+		Ejercicio3 e = new Ejercicio3();
+		e.eje3Inicia();
 		
 		
 		
