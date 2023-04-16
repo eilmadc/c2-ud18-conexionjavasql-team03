9d5ef05cdcc21c9099448b90449fd4b963ebc20b
eilmadc@gmail.com <eilmadc@gmail.com>
2023-04-16 02:23:21 +0200

    Update Database.java

--- c2-ud18-conexionjavasql-team03/src/database/Database.java
+++ c2-ud18-conexionjavasql-team03/src/database/Database.java
@@ -5,55 +5,62 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-
 public class Database {
-//-----------------------------------------------------------------
+
+	/*
+	 * ------------------------------Open
+	 * connection-----------------------------------
+	 */
 	public Connection openConnection(Connection conexion) {
-		//Connection conexion = null;
+		// Connection conexion = null;
 		conexion = null;
 		try {
 
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			conexion = DriverManager.getConnection("jdbc:mysql://192.168.1.43:3306", "remote", "-Crocodile123");
+			conexion = DriverManager.getConnection("jdbc:mysql://192.168.1.230:3306", "remote", "F3d0r@...");
 			System.out.println("Conectado a la base de datos");
 
 		} catch (SQLException | ClassNotFoundException e) {
-			// TODO Auto-generated catch block
 			System.out.println(123321);
-			//e.printStackTrace();
+			// e.printStackTrace();
 		}
 
 		return conexion;
 	}
 
-	//-----------------------------------------------------------------
+	/*
+	 * ------------------------------Create
+	 * Database-----------------------------------
+	 */
 	public void createDatabase(String nom, Connection conexion) {
 
 		try {
 			String Query = "CREATE DATABASE " + nom;
 			Statement st = conexion.createStatement();
 			st.executeUpdate(Query);
-			// closeConnection(conexion);
 			openConnection(conexion);
-			// openConnection("root", "", nom);
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			System.out.println("Error abriendo la conexion " + e);
 		}
 	}
 
-	//-----------------------------------------------------------------
+	/*
+	 * ------------------------------Close
+	 * connection-----------------------------------
+	 */
 	public void closeConnection(Connection conexion) {
 		// TODO Auto-generated method stub
 		try {
 			conexion.close();
 			System.out.println("Cerrada la conexion");
 		} catch (SQLException e) {
-			System.out.println("Error cerrando la conexion: "+ e);
+			System.out.println("Error cerrando la conexion: " + e);
 		}
 	}
 
-	//-----------------------------------------------------------------
+	/*
+	 * ------------------------------Create table-----------------------------------
+	 */
 	public void createTable(String db, String table, String atributos, Connection conexion) {
 		try {
 			// USE database
@@ -64,15 +71,16 @@ public class Database {
 			// CREATE TABLE
 			Statement st = conexion.createStatement();
 			st.executeUpdate("CREATE TABLE " + table + "(" + atributos + ");");
-			System.out.println("Table creada: "+table);
+			System.out.println("Table creada: " + table);
 
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			System.out.println("Error creando la table: "+ e);
+			System.out.println("Error creando la table: " + e);
 		}
 	}
 
-	//-----------------------------------------------------------------
+	/*
+	 * ------------------------------Insert Data-----------------------------------
+	 */
 	public void insertData(String db, String table, String atributos, Connection conexion) {
 		try {
 			// USE database
@@ -83,15 +91,16 @@ public class Database {
 			// CREATE TABLE
 			Statement st = conexion.createStatement();
 			st.executeUpdate("INSERT INTO " + table + " VALUE(" + atributos + ");");
-			System.out.println("data insertada: "+ atributos);
+			System.out.println("data insertada: " + atributos);
 
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			System.out.println("Error insertando data: "+e);
+			System.out.println("Error insertando data: " + e);
 		}
 	}
 
-	//-----------------------------------------------------------------
+	/*
+	 * ------------------------------Get Values-----------------------------------
+	 */
 	public java.sql.ResultSet getValues(String db, String table, Connection conexion) {
 		java.sql.ResultSet resultSet = null;
 		try {
@@ -105,12 +114,15 @@ public class Database {
 			resultSet = stsel.executeQuery(querySelect);
 
 		} catch (SQLException e) {
-			System.out.println("Values no coleccionadas correctamente:" +e);
+			System.out.println("Values no coleccionadas correctamente:" + e);
 		}
 		return resultSet;
 	}
 
-	public void deleteDatabase(String db, String table, String ID,String IDValor, Connection conexion) {
+	/*
+	 * ------------------------------Delete Database-----------------------------------
+	 */
+	public void deleteDatabase(String db, String table, String ID, String IDValor, Connection conexion) {
 		try {
 
 			String query = "DELETE FROM " + table + " WHERE " + ID + " = " + IDValor;
@@ -123,12 +135,15 @@ public class Database {
 		}
 	}
 
+	/*
+	 * ------------------------------drop database-----------------------------------
+	 */
 	public void dropDatabase(String db, Connection conexion) {
 		try {
 			String queryDB = "DROP DATABASE IF EXISTS " + db;
 			Statement stdb = conexion.createStatement();
 			stdb.executeUpdate(queryDB);
-			
+
 		} catch (SQLException ex) {
 			System.out.println("Drop incomplete");
 		}
